39ca72b0f171fef6ee312463cfb446eed8ea2ee7
KIKS <keiji.kobushi@gmail.com>
2026-07-12 20:48:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ with st.sidebar:
 st.title("🎓 新入生のための系列選択ナビ")
 
 # プログレスバー
-steps_map = {'IE': 10, 'IIE': 5, 'NOT': 5, 'Q1': 19, 'Q2': 28,'Q3': 37, 'Q4': 46, 'Q5': 55, 'Q6': 64, 'Q7': 73, 'Q8': 82, 'Q9': 91, 'suki': 99, 'tiga': 0, 'tya': 0, 'goal': 100}
+steps_map = {'IE': 10, 'IIE': 5, 'NOT': 5, 'Q1': 18, 'Q2': 26,'Q3': 34, 'Q4': 42, 'Q5': 50, 'Q6': 58, 'Q7': 66, 'Q8': 74, 'Q9': 82, 'Q10': 90 'suki': 99, 'tiga': 0, 'tya': 0, 'goal': 100}
 if st.session_state.step in steps_map:
     progress = steps_map[st.session_state.step]
     st.progress(progress / 100)
